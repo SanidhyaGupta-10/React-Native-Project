d8f3288c94e12f7b809628a55e18ef5047732826
Sanidhya Gupta <adityasgupta1857@gmail.com>
2026-01-19 19:47:57 +0530

    Header Component Fixed

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,50 @@
-import { Text, View } from 'react-native'
+import createHomeStyles from '@/app/styles/home.style'
+import { api } from '@/convex/_generated/api'
+import { useTheme } from "@/hooks/useTheme";
+import { Ionicons } from '@expo/vector-icons';
+import { useQuery } from 'convex/react'
+import { LinearGradient } from 'expo-linear-gradient';
+import { View, Text } from 'react-native'
 
 const Header = () => {
-  return (
-    <View>
-      <Text>Header</Text>
-    </View>
-  )
-}
+  const { colors } = useTheme();
+  const homeStyles = createHomeStyles(colors);
 
-export default Header;
+  const todos = useQuery(api.todos.getTodos);
+
+  const completedCount = todos ? todos.filter((todo) => todo.isComplete).length : 0;
+  const totalCount = todos ? todos.length : 0;
+  const progressPercentage = totalCount > 0 ? (completedCount / totalCount) * 100 : 0;
+
+  return (
+    <View style={homeStyles.header}>
+      <View style={homeStyles.titleContainer}>
+        <LinearGradient colors={colors.gradients.primary} style={homeStyles.iconContainer}>
+          <Ionicons name='flash-outline' size={28} color='#fff' />
+        </LinearGradient>
+
+        <View style={homeStyles.titleTextContainer}>
+          <Text style={homeStyles.title}>Today&apos;s Tasks</Text>
+          <Text style={homeStyles.subtitle}>
+            {completedCount} of {totalCount} completed
+          </Text>
+        </View>
+      </View>
+
+          <View style={homeStyles.progressContainer}>
+            <View style={homeStyles.progressBarContainer}>
+              <View style={homeStyles.progressBar}>
+                <LinearGradient
+                colors={colors.gradients.success}
+                style={[homeStyles.progressFill, { width: `${progressPercentage}%` }]}
+                />
+                </View>
+                  <Text style={homeStyles.progressText}>{Math.round(progressPercentage)}%</Text>
+            
+            </View>
+          </View>
+    </View>
+  );
+};
+
+export default Header
